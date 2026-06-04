eb6fc6d43792613968e4c2813f70884709f42670
8-prime <hoffmeister.stefan99@gmail.com>
2026-06-04 23:26:42 +0200

    fix: formatting

--- src/Transcodarr.Node/Services/NodeState/CapabilitiesService.cs
+++ src/Transcodarr.Node/Services/NodeState/CapabilitiesService.cs
@@ -54,7 +54,7 @@ public class CapabilitiesService(
     public async Task<(
         List<EncoderCapability> Encoders,
         Dictionary<EncoderGroup, int> GroupCapacities
-        )> GetEncodersAsync(CancellationToken stoppingToken)
+    )> GetEncodersAsync(CancellationToken stoppingToken)
     {
         var encoders = new List<EncoderCapability>();
         foreach (var kvp in TranscodersMapping.EncodersByCodec)
